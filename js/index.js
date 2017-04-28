2eaae98aa650ba2aab0a886ba725db0d4565a3b6
DESKTOP-SJU8620\user1 <dlindner999@gmail.com>
2017-04-28 14:21:10 -0600

    4.28 2pm - all functions and extra credit working. Push prior to code cleanup.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function newBook(author, title, numberOfPages, publishedDate, series, seriesOrder){   //Constructor function - main prototype to inherit
+ function newBook(author, title, numberOfPages, publishedDate, series, seriesOrder){   //Constructor function
   this.author = author;
   this.title = title;
   this.numberOfPages = numberOfPages;
@@ -7,37 +7,41 @@ function newBook(author, title, numberOfPages, publishedDate, series, seriesOrde
   this.seriesOrder = seriesOrder;
 }
 
-var myLibrary = function(){
+var myLibrary = function(){};
 
-};
+//my book objects
+var myNewBook1 = new newBook("John Sandford", "Shock Wave", 427, new Date("01/26/2009"), "Virgil Flowers", 3);
+var myNewBook2  = new newBook("William Golding", "Lord of the Flies", 443, new Date("10/04/1959"), "", 0);
+var myNewBook3 = new newBook("John Sandford", "Heat Lightning", 361, new Date("10/02/2007"), "Virgil Flowers", 1);
+var myNewBook4 = new newBook("John Sandford", "Bad Blood", 396, new Date("12/16/2008"), "Virgil Flowers", 2);
+var myNewBook5 = new newBook("Stephen King", "The Gunslinger", 601, new Date("07/24/1983"), "The Dark Tower", 1);
+var myNewBook6  = new newBook("Stephen King", "The Waste Lands", 721, new Date("03/04/1992"), "The Dark Tower", 3);
+var myNewBook7  = new newBook("Chuck Palahniuk", "Fight Club", 426, new Date("08/28/1996"), "", 0);
+var myNewBook8 = new newBook("Stephen King", "Wizard and Glass", 787, new Date("11/04/1997"), "The Dark Tower", 4);
+var myNewBook9 = new newBook("Stephen King", "The Drawing of the Three", 541, new Date("03/06/1987"), "The Dark Tower", 2);
+var myNewBook10  = new newBook("J.R.R Tolkein", "The Hobbit", 640, new Date("01/09/1951"), "", 0);
+
 //Instance of my library object, using the myLibrary prototype.
 var testLibrary = new myLibrary();
 
-var myNewBook1 = new newBook("John Sandford", "Rules of Prey", 301, new Date("07/24/1989"), "Lucas Davenport", 1);
-var myNewBook2 = new newBook("John Sandford", "Shadow Prey", 349, new Date("02/28/1990"), "Lucas Davenport", 2);
-var myNewBook3 = new newBook("John Sandford", "Eyes of Prey", 441, new Date("10/21/1990"), "Lucas Davenport", 3);
-var myNewBook4 = new newBook("John Sandford", "Heat Lightning", 361, new Date("10/02/2007"), "Virgil Flowers", 1);
-var myNewBook5 = new newBook("John Sandford", "Bad Blood", 396, new Date("12/16/2008"), "Virgil Flowers", 2);
-var myNewBook6 = new newBook("John Sandford", "Shock Wave", 427, new Date("01/26/2009"), "Virgil Flowers", 3);
-var myNewBook7 = new newBook("Stephen King", "The Gunslinger", 601, new Date("07/24/1983"), "The Dark Tower", 1);
-var myNewBook8 = new newBook("Stephen King", "The Drawing of the Three", 541, new Date("03/06/1987"), "The Dark Tower", 2);
-var myNewBook9 = new newBook("Stephen King", "Wizard and Glass", 787, new Date("11/04/1997"), "The Dark Tower", 4);
-var myNewBook10 = new newBook("Stephen King", "Wolves of the Calla", 569, new Date("11/04/2005"), "The Dark Tower", 5);
-var myNewBook11 = new newBook("Stephen King", "The Waste Lands", 721, new Date("03/04/1992"), "The Dark Tower", 3);
-var myNewBook12 = new newBook("Stephen King", "Song of Susannah", 647, new Date("08/14/2006"), "The Dark Tower", 6);
-var myNewBook13 = new newBook("Stephen King", "The Wind Through the Keyhole", 335, new Date("05/06/2012"), "The Dark Tower", 8);
-var myNewBook14 = new newBook("Stephen King", "Christine", 601, new Date("07/24/1989"), "", 0);
-var myNewBook15 = new newBook("Chuck Palahniuk", "Fight Club", 426, new Date("08/28/1996"), "", 0);
-var myNewBook16 = new newBook("James Rollins", "The Seventh Plague", 426, new Date("12/13/2016"), "Sigma Force", 12);
-var myNewBook17 = new newBook("Stephen King", "The Dark Tower", 876, new Date("09/30/2007"), "The Dark Tower", 7);
-var myNewBook18 = new newBook("John Sandford", "Golden Prey", 436, new Date("07/24/2017"), "Lucas Davenport", 24);
-var myNewBook19 = new newBook("Richard Bachman", "Blaze", 498, new Date("06/12/2001"), "", 0);
-var myNewBook20 = new newBook("J.K. Rowling", "Harry Potter and the Deathly Hallows", 843, new Date("10/14/2010"), "Harry Potter", 7);
-var myNewBook21 = new newBook("William Golding", "Lord of the Flies", 443, new Date("10/04/1959"), "", 0);
-var myNewBook22 = new newBook("J.R.R Tolkein", "The Hobbit", 640, new Date("01/09/1951"), "", 0);
+myLibrary.prototype.myArray = [];
 
+myLibrary.prototype.myBookShelf = [];
+testLibrary.myBookShelf = [myNewBook1, myNewBook2, myNewBook3, myNewBook4, myNewBook5, myNewBook6, myNewBook7, myNewBook8, myNewBook9, myNewBook10];
+//console.log(testLibrary.myBookShelf);
 
-myLibrary.prototype.myArray = new Array();
+// myLibrary.prototype.bookshelf = [];
+//
+//
+// //Purpose: add all books in the library to myArray
+// myLibrary.prototype.createBookshelf = function(){
+// debugger;
+//   //  this.bookshelf.push(myNewBook1);
+// var bookshelf=[];
+// for(int x = 0; x < ; x++)
+// {
+//   this.push(this.myNewBook+x)
+// }
 
 //Purpose: Add a book object to your books array.
 //Return: boolean true if it is not already added, false if it is already added.
@@ -64,11 +68,6 @@ myLibrary.prototype.addBook = function(myNewBook){
   }
 }
 // testLibrary.addBook(myNewBook1);
-// testLibrary.addBook(myNewBook18);
-// testLibrary.addBook(myNewBook2);
-// testLibrary.addBook(myNewBook12);
-// testLibrary.addBook(myNewBook16);
-
 
 
 //Purpose: Remove book from from the books array by its title.
@@ -194,15 +193,17 @@ myLibrary.prototype.addBooks = function(books){
     return 0;
   }
 }
-testLibrary.addBooks([myNewBook1, myNewBook10, myNewBook16, myNewBook4, myNewBook7, myNewBook15, myNewBook19, myNewBook20, myNewBook21, myNewBook22]);
 
+// testLibrary.addBooks([myNewBook1, myNewBook2, myNewBook3, myNewBook4, myNewBook5, myNewBook6, myNewBook7, myNewBook8, myNewBook9, myNewBook10, myNewBook11, myNewBook12, myNewBook13,
+// myNewBook14, myNewBook15, myNewBook16, myNewBook17, myNewBook18, myNewBook19, myNewBook20, myNewBook21, myNewBook21]);
+testLibrary.addBooks(this.myBookShelf);
 
 //Purpose: Find the distinct authors’ names from all books in your library.
 //Return: array of strings the names of all distinct authors, empty array if no books exist or if no authors exist.
 myLibrary.prototype.getAuthors = function(){
   var authorsArray = [];
   var blnAuthorMatch = false;
-  for(var i = 0; i < this.myArray.length; i++){   //loop through each book in the library to get it's author.
+  for(var i = 0; i < this.myArray.length; i++){   //loop through each book in the library to get its author.
     blnAuthorMatch = false;
     if(authorsArray.length > 0){
       for(var j = 0; j < authorsArray.length; j++){   //loop through all the author names that have already been added.
@@ -217,7 +218,6 @@ myLibrary.prototype.getAuthors = function(){
     authorsArray.push(this.myArray[i].author);  //if the list of authors is empty, add the author.
     }
   }
-
   if(authorsArray.length > 0){
     console.log(authorsArray.length + " authors found in the library.");
   }
@@ -240,12 +240,64 @@ myLibrary.prototype.getRandomAuthorName = function(){
 }
 
 //Extra credit ******************************************************************************************************
-//Purpose: Retrieves a book series, in series order.
-//Return: array of book objects, empty array if series doesn't exist.
-myLibrary.prototype.getSeriesInOrder = function(){
-    var seriesArray = [];
-    if(testLibrary.myArray.length > 0){
 
+//Purpose: Retrieves a distinct list of the names of all series in the the library.
+//Return: array of strings the names of all distinct series, empty array if no books exist or if no series exist.
+myLibrary.prototype.getSeries = function(){
+  var seriesArray = [];
+  var blnSeriesMatch = false;
+  for(var i = 0; i < this.myArray.length; i++){   //loop through each book in the library to get its series.
+    blnSeriesMatch = false;
+    if(seriesArray.length > 0){
+      for(var j = 0; j < seriesArray.length; j++){  //loop through all the series names that have already been added.
+        console.log(seriesArray[j]);
+        console.log(this.myArray[i].series);
+        console.log(seriesArray[j].toLowerCase() == this.myArray[i].series.toLowerCase());
+        if(seriesArray[j].toLowerCase() == this.myArray[i].series.toLowerCase()){   //check if the series name would be a duplicate.
+          blnSeriesMatch = true;  //a duplicate was found. Don't add this series.
+          console.log("blnSeriesMatch: " + blnSeriesMatch);
+        }
+      }
+      if(blnSeriesMatch == false){  //don't add if series name is "" or was already added.
+        if(this.myArray[i].series != ""){
+          seriesArray.push(this.myArray[i].series);   //if the series isn't already in the list, add the author.
+        }
+      }
+    } else {
+      if(this.myArray[i].series != ""){   //don't add if series name is ""
+        seriesArray.push(this.myArray[i].series);   //if the list of series is empty, add the author.
+      }
     }
-      return seriesArray;
+  }
+  if(seriesArray.length > 0){
+    console.log(seriesArray.length + " series found in the library.");
+  }
+  return seriesArray;
 }
+
+
+//Purpose: Retrieves all books in the series name passed into the function, in series order.
+//Return: array of book objects, empty array if series doesn't exist.
+myLibrary.prototype.getSeriesInOrder = function(seriesName){
+    var seriesArray = [];
+    // var orderArray = [];
+
+    if(testLibrary.myArray.length > 0){
+      for(var i = 0; i < this.myArray.length; i++){
+        if(this.myArray[i].series == seriesName){
+          seriesArray.push(this.myArray[i]);
+          // orderArray.push(this.myArray[i].seriesOrder);
+        }
+      }
+      seriesArray.sort(function(a, b){
+        return a.seriesOrder - b.seriesOrder;
+      });
+      console.log(seriesArray);
+    }
+    for(var j = 0; j < seriesArray.length; j++){
+      console.log(seriesArray[j].seriesOrder + ". " + seriesArray[j].title);
+    }
+    return seriesArray;
+}
+
+// testLibrary.getSeriesInOrder("The Dark Tower");
